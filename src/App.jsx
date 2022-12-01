45e4b117de5a58e326bac1eb1ff7198d9338771c
Sebastián-Markoja <sebaacc11@gmail.com>
2022-12-01 17:57:49 -0300

    Agregamos BrowserRouter en index.jsx, las rutas de los componentes en App.jsx, los links dentro del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home"
+import Contact from "./Routes/Contact"
+import Detail from "./Routes/Detail"
+import Favs from "./Routes/Favs"
+import { Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Favs" element={<Favs />} />
+        <Route path="/dentist/:id" element={<Detail />} />
+      </Routes>
+      <Footer />
+
+    </div>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -7,6 +8,10 @@ const Navbar = () => {
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      {/*estoy agregando links para las rutas*/}
+      <Link to='/'><h3>Home</h3></Link>
+      <Link to='/Contact'><h3>Contact</h3></Link>
+      <Link to='/Favs'><h3>Favs</h3></Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
